a23a6fcdcc6539c1bfa381a1077ae8690b1f9ffe
Dallas Read <dallas@excitereative.ca>
2014-04-08 02:29:55 -0300

    CSS and JS.

--- assets/js/instagive.js
+++ assets/js/instagive.js
@@ -3,24 +3,29 @@
     setPageDimensions: function() {
       var height;
       ig.mobile = $(window).width() < 600;
+      ig.topbar_height = $(".topbar").height();
       if ($("#post").length) {
         ig.avatar_offset = $("#post .avatar").offset().top;
-        ig.topbar_height = $(".topbar").height();
       }
       if ($(".sidebar").length) {
         height = $(".yield").height() + 60;
         $(".sidebar").height(height);
       }
+      if ($("#pricing").length) {
+        $("#pricing th").each(function() {
+          return $(this).css("width", $(this).width());
+        });
+      }
       return $(window).scroll();
     },
-    setAvatar: function() {
-      var padding, scrolled;
+    setAvatar: function(scrolled) {
+      var content_top_border_height, padding;
       if ($("#post").length) {
-        scrolled = $(window).scrollTop();
         padding = 30;
-        if (ig.avatar_offset < scrolled + ig.topbar_height + 30) {
+        content_top_border_height = 15;
+        if (ig.avatar_offset < scrolled + ig.topbar_height + content_top_border_height) {
           return $("#post .author").addClass("fixed").css({
-            top: ig.topbar_height + 30
+            top: ig.topbar_height + content_top_border_height
           });
         } else {
           return $("#post .author").removeClass("fixed").css({
@@ -29,14 +34,44 @@
         }
       }
     },
+    setPricingHeader: function(scrolled) {
+      var placeholder;
+      if ($("#pricing_placeholder").length) {
+        placeholder = $("#pricing").offset().top;
+        if (scrolled + ig.topbar_height < placeholder - 48) {
+          $("#pricing_placeholder").hide();
+          return $("#pricing").removeClass("fixed_header");
+        } else if (scrolled + ig.topbar_height > placeholder) {
+          $("#pricing_placeholder").show();
+          return $("#pricing").addClass("fixed_header");
+        }
+      }
+    },
+    scroll: function() {
+      var scrolled;
+      scrolled = $(window).scrollTop();
+      ig.setAvatar(scrolled);
+      return ig.setPricingHeader(scrolled);
+    },
     load: function() {
       return ig.setPageDimensions();
     }
   };
 
+  $(document).on({
+    mouseenter: function() {
+      var index;
+      index = $(this).index() + 1;
+      return $("#pricing").find("td:nth-child(" + index + "):not(.trait)").addClass("hover");
+    },
+    mouseleave: function() {
+      return $("#pricing").find(".hover").removeClass("hover");
+    }
+  }, "#pricing th, #pricing td");
+
   $(function() {
     setTimeout(ig.setPageDimensions, 1500);
-    $(window).scroll(ig.setAvatar);
+    $(window).scroll(ig.scroll);
     return $(window).resize(ig.setPageDimensions);
   });
 
